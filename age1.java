5cf59b58df40cff44e271eb0f3899b25325ebd68
rajgautam1 <86864142+rajgautam1@users.noreply.github.com>
2021-08-25 11:32:52 -0400

    Add files via upload
    Eclipse

---
+++ age1.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class age1 {
+
+	public static void main(String[] args) {
+		System.out.println("How old are you ?");
+Scanner scan = new Scanner(System.in);
+int age = scan.nextInt();
+
+System.out.println("Age : "  + age);
+	}
+
+}
